7e840a02cb1d086e2d3050a5f32e20e619445546
AveryCohen <AveryCohen04@gmail.com>
2022-04-07 11:47:28 -0400

    finished shape inheritance

--- src/ShapeInheritance.java
+++ src/ShapeInheritance.java
@@ -4,7 +4,14 @@ public class ShapeInheritance extends Shape {
         Circle c = new Circle(10, "pink", true);
         Triangle t = new Triangle(10, 10, 10, "pink", true);
         Rectangle r = new Rectangle(10, 10, "pink", true);
-        Shape[] shapeList = new Shape[];
+        Shape[] shapeList = new Shape[3];
+        shapeList[0] = c;
+        shapeList[1] = t;
+        shapeList[2] = r;
+        for (int i = 0; i < shapeList.length; i++) {
+            double area = shapeList[i].getArea();
+            System.out.println(area);
+        }
     }
 
 }
